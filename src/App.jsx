4ccadbfe75199b6dc1c7e8ba519e25c8371bd987
madhurajituri <jiturimadhura@gmail.com>
2024-02-21 18:52:51 +0530

    Initial commit of files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <body className="mx-auto w-full h-full bg-blue-100">
+
+      <div ></div>
+    </body>
+    </>
+  )
+}
+
+export default App
