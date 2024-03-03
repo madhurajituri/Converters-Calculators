3a6d044e0dfd624c5d01d0267e85c3239dfcbcd8
madhurajituri <jiturimadhura@gmail.com>
2024-03-03 16:26:11 +0530

    yaml to json done

--- src/Json.jsx
+++ src/Json.jsx
@@ -1,6 +1,30 @@
-function Json(){
-    return(
-        <p>Json</p>
+import { React, useState } from "react";
+import YAML from 'js-yaml';
+
+function Json() {
+
+    const [json, setjson] = useState("");
+    const [yaml, setyaml] = useState("");
+
+    function convert() {
+        const yamlObject = YAML.load(yaml);
+        const x = JSON.stringify(yamlObject, null, 2);
+        setjson(x);
+    }
+
+    return (
+        <div>
+            <div className="heading">YAML to JSON</div>
+            <div className="m-10 gap-5 justify-center flex">
+                <div className="items-center gap-2 flex flex-col">
+                    <textarea className="codearea w-[700px]" value={yaml} onChange={e => setyaml(e.target.value)}></textarea>
+                    <button className="convertbtn w-40" onClick={convert}>Convert</button>
+                </div>
+                <div>
+                    <textarea className="codearea w-[700px]" value={json} readOnly></textarea>
+                </div>
+            </div>
+        </div>
     )
 }
 
